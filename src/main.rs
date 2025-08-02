f0044733e349b76eaad99a63dabad2ad494b43e5
Ahmad Alsaleh <ahmed.asaleh2@gmail.com>
2025-08-02 16:38:16 +0400

    add basic implementation

--- src/main.rs
+++ src/main.rs
@@ -13,18 +13,6 @@ struct Args {
     file: String,
 }
 
-// TODO: support negative indexing (like in Python)
-// TODO: support a flag `--force` to print last line in case
-// N is beyond the max lines and first line if line is negative
-// TODO: add a flag to print to stderr instead of stdout
-// TODO: use pretty print (consider using olive!), something similar to `bat` style, but add a flag
-// to make it plain, and make it plain by default if redirection is detected (check how bat does
-// that)
-// TODO: add a flag to switch zero/one index
-// TODO: write test cases
-// TODO: add syntax for range (eg: -n 1:4)
-// TODO: add syntax for multiple lines (eg: -n 1,4)
-
 fn main() -> anyhow::Result<()> {
     let args = Args::parse();
 
