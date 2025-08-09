a3a9d8e0d4bdcb924dca9eda1cebe1073fe0632c
Ahmad Alsaleh <ahmed.asaleh2@gmail.com>
2025-08-09 22:55:56 +0400

    move line reading logic into a function

--- src/main.rs
+++ src/main.rs
@@ -10,31 +10,19 @@ mod cli;
 mod line_reader;
 
 fn main() -> Result<()> {
-    let mut args = Cli::parse();
+    let args = Cli::parse();
 
     if args.line == 0 {
         anyhow::bail!("Line number can't be zero");
     }
 
-    // the cli user uses one-index but the code uses zero-index
-    args.line -= 1;
-
     let file = open_file(&args.file)?;
-    let mut line_reader = LineReader::new(file).context("Failed to create file reader")?;
+    let line_reader = LineReader::new(file).context("Failed to create file reader")?;
 
-    let mut line = Vec::new();
-    let line_in_range = line_reader
-        .read_specific_line(&mut line, args.line)
-        .with_context(|| format!("Failed to read line {}", args.line + 1))?;
-    if !line_in_range {
-        anyhow::bail!(
-            "Line {} is out of bound, file has {} line only",
-            args.line + 1,
-            line_reader.current_line
-        );
-    }
+    let line = read_line(args.line, line_reader)?;
 
     std::io::stdout()
+        .lock()
         .write_all(&line)
         .context("Failed to write line to stdout")?;
 
@@ -52,4 +40,21 @@ fn open_file(path: &Path) -> Result<File> {
         anyhow::bail!("{} is not a file", path.display());
     }
     Ok(file)
+}
+
+fn read_line(line: usize, mut line_reader: LineReader) -> Result<Vec<u8>> {
+    let mut buf = Vec::new();
+    let line_in_range = line_reader
+        // subtract one since the cli user uses one-index and the code uses zero-index
+        .read_specific_line(&mut buf, line - 1)
+        .with_context(|| format!("Failed to read line {line}"))?;
+
+    if !line_in_range {
+        anyhow::bail!(
+            "Line {line} is out of bound, file has {} line only",
+            line_reader.current_line
+        );
+    }
+
+    Ok(buf)
 }
