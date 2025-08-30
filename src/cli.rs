30b1b905b3cf0555301e30e5fd450c990c57c4b2
Ahmad Alsaleh <ahmed.asaleh2@gmail.com>
2025-08-30 20:46:06 +0400

    add `-c`, `-a`, and `-b` flag to `src/cli.rs`

--- src/cli.rs
+++ src/cli.rs
@@ -2,15 +2,13 @@ use crate::line_selector::OriginalLineSelector;
 use clap::Parser;
 use std::path::PathBuf;
 
+// TODO: consider using https://github.com/Canop/clap-help
 #[derive(Parser, Debug)]
-#[command(version, about="Extract lines without hacks", author, long_about = None)]
+#[command(version, about="Extract lines without hacks", author, long_about = None, next_line_help = true)]
 pub(crate) struct Cli {
     /// Line number(s) to extract
-    #[arg(short = 'n', long = "line", name = "LINE-SELECTORS", value_parser = OriginalLineSelector::from_str, value_delimiter = ',', required = true)]
+    #[arg(short = 'n', long = "line", value_name = "LINE-SELECTORS", value_parser = OriginalLineSelector::from_str, value_delimiter = ',', required = true)]
     pub(crate) original_line_selectors: Vec<OriginalLineSelector>,
-
-    /// File to extract line(s) from. Use a dash ('-') or no argument to read from standard input
-    pub(crate) file: PathBuf,
 
     /// Treat binary files as text files
     #[arg(long)]
@@ -19,4 +17,28 @@ pub(crate) struct Cli {
     ///Only show plain style, no decorations or line numbers
     #[arg(short, long)]
     pub(crate) plain: bool,
+
+    /// Show N lines before the selected line
+    #[arg(long, short, value_name = "N", default_value_t = 0)]
+    pub(crate) before: usize,
+
+    /// Show N lines after the selected line
+    #[arg(long, short, value_name = "N", default_value_t = 0)]
+    pub(crate) after: usize,
+
+    /// Show context lines around the selected line
+    /// Equivalent to setting both --after and --before to the same value
+    #[arg(
+        long,
+        short,
+        default_value_t = 0,
+        conflicts_with = "before",
+        conflicts_with = "after",
+        value_name = "N",
+        verbatim_doc_comment
+    )]
+    pub(crate) context: usize,
+
+    /// File to extract line(s) from. Use a dash ('-') or no argument to read from standard input
+    pub(crate) file: PathBuf,
 }
